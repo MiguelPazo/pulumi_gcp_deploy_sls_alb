bd126f4a0eb3a2a7857f84c5c4fcf2236f19c11f
Miguel Pazo <miguel.ps19@gmail.com>
2021-04-24 19:27:09 -0500

    custom powered

--- index.ts
+++ index.ts
@@ -84,6 +84,7 @@ for (let slsFunction of slsConfigs.resources) {
             "Content-Security-Policy: frame-ancestors 'self'",
             'Strict-Transport-Security: max-age=31536000; includesubdomains',
             'X-Content-Type-Options: nosniff',
+            'X-Powered-By: Java 8',
             'Cache-Control: no-cache="Set-Cookie"',
         ]
     });
